d6eee30c0e56a2e27c21d37eabbc9e81ca64647b
Gianluca <gl.galassini@gmail.com>
2021-09-18 02:24:00 +0200

    inizio main

---
+++ laravel-/resources/js/components/Main.vue
@@ -0,0 +1,57 @@
+<template>
+    <main class="container">
+        <div class="row">
+            <div class="col-6">
+                <div class="card text-center m-5">
+                <div class="card-header">
+                    categoria
+                </div>
+                <div class="card-body">
+                    <h5 class="card-title">titolo</h5>
+                    <p class="card-text">contenuto Lorem ipsum dolor sit amet, consectetur adipisicing elit. Numquam asperiores illo aspernatur eum dolore officia quia harum impedit vitae eos.</p>
+                    <a href="#" class="btn btn-primary">dettagli</a>
+                </div>
+                <div class="card-footer text-muted">
+                    tags
+                </div>
+            </div>
+            </div>
+            <div class="col-6">
+                <div class="card text-center m-5">
+                <div class="card-header">
+                    categoria
+                </div>
+                <div class="card-body">
+                    <h5 class="card-title">titolo</h5>
+                    <p class="card-text">contenuto Lorem ipsum dolor sit amet, consectetur adipisicing elit. Numquam asperiores illo aspernatur eum dolore officia quia harum impedit vitae eos.</p>
+                    <a href="#" class="btn btn-primary">dettagli</a>
+                </div>
+                <div class="card-footer text-muted">
+                    tags
+                </div>
+            </div>
+            </div>
+        </div>
+    </main>
+</template>
+
+<script>
+export default{
+    name: "Main",
+    data() {
+        return{
+            call: 'http://localhost:8000/api/posts'
+        }
+    },
+    created(){
+
+    },
+    methods(){
+        
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- laravel-/resources/js/front.js
+++ laravel-/resources/js/front.js
@@ -1,6 +1,7 @@
 require('./bootstrap');
 
 window.Vue = require('vue');
+window.axios =require('axios');
 
 import Vue from 'vue';
 import App from './views/App';
